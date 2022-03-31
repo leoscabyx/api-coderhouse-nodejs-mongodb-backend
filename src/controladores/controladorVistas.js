d88b62d28fdf7c9d53f38e7a68eb4bbdc5473d45
Leonardo Lovera <ing.leonardo.lovera@gmail.com>
2022-03-31 13:37:53 -0300

    Implementando apartado para el endpoint de /info y /chat con sus respectivas vistas y demas funcionalidades de la entrega final

--- src/controladores/controladorVistas.js
+++ src/controladores/controladorVistas.js
@@ -1,76 +1,25 @@
-import { instanciasDaos } from '../daos/index.js'
-import logger from '../logger.js'
-const DaoUsuarios = instanciasDaos.DaoUsuarios
-const DaoUsuarios2 = instanciasDaos.DaoUsuarios
-const DaoProductos = instanciasDaos.DaoProductos
-const DaoCarritos = instanciasDaos.DaoCarritos
+import os from 'os'
+const numCPUs = os.cpus().length
 
-async function getViewInicio (req, res) {
-    if(req.user){
-        res.render('index', { page: 'Inicio', auth: true, nombre: req.user.username }) 
-    }else{
-        res.render('index', { page: 'Inicio', auth: false })
+async function getViewInfo (req, res) {
+    const processDetail = {
+        argumentos: process.argv.slice(2).length > 0 ? process.argv.slice(2).join(',') : 'Sin argumentos por terminal',
+        plataforma: process.platform,
+        versionNode: process.version,
+        memoriaTotal: process.memoryUsage().rss,
+        carpetaProyecto: process.cwd(),
+        pathEjecucion: process.execPath,
+        cpus: numCPUs,
+        pid: process.pid
     }
+    res.render('info', { page: 'Detalle Servidor en Node', data: processDetail })
 }
 
-async function getViewProductos (req, res) {
-    const productos = await DaoProductos.getAll()
-    res.render('products', { page: 'Productos', productos })
-}
-
-async function getViewCarrito (req, res) {
-    const carritoUser = await DaoCarritos.getCarritoUser(req.user.id)
-    let productos = []
-    if(carritoUser){
-        productos = [...carritoUser.productos]
-    }
-
-    res.render('cart', { page: 'Carrito', productos })
-}
-
-async function postViewCarrito (req, res) {
-    const carritoUser = await DaoCarritos.getCarritoUser(req.user.id)
-    const { id, title, description, code, price, thumbnail, stock } = await DaoProductos.getById(req.body.idProducto)
-    const producto = {
-        id,
-        timestamp: Date.now(), 
-        title,
-        description,
-        code,
-        price, 
-        thumbnail,
-        stock
-    }
-    
-    if(!carritoUser){
-        const idCarritoUser = await DaoCarritos.save({ productos: [], idUser: req.user.id })
-        await DaoCarritos.saveProduct(idCarritoUser, producto)
-    }else{
-        await DaoCarritos.saveProduct(carritoUser.id, producto)
-    }
-
-    res.redirect('/productos')
-}
-
-async function getViewCheckout (req, res) {
-    const dataUser = await DaoUsuarios.getById(req.user.id)
-    const carritoUser = await DaoCarritos.getCarritoUser(req.user.id)
-    await DaoCarritos.notificarCarrito(carritoUser, dataUser)
-    await DaoCarritos.deleteById(carritoUser.id)
-    res.render('checkout', { page: 'Checkout', username: req.user.username })
-}
-
-async function getViewPerfil (req, res) {
-    const dataUser = await DaoUsuarios.getById(req.user.id)
-    logger.info(DaoUsuarios === DaoUsuarios2, "Comprobando si es la misma instancia")
-    res.render('profile', { page: 'Perfil', perfil: dataUser })
+async function getViewChat (req, res) {
+    res.render('chat', { page: 'WebSockets en Node' })
 }
 
 export {
-    getViewInicio,
-    getViewProductos,
-    getViewCarrito,
-    postViewCarrito,
-    getViewCheckout,
-    getViewPerfil
+    getViewInfo,
+    getViewChat,
 }
